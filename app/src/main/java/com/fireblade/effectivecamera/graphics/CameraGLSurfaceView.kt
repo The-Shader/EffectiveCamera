e8c2a43d6c29593bbcccc0d548db3f5890a1556d
David Tverdota <fireblade.sg@gmail.com>
2019-10-26 22:04:44 +0100

    Propagated the data from the ViewModel to the Renderer and enabled it within the shader.

--- app/src/main/java/com/fireblade/effectivecamera/graphics/CameraGLSurfaceView.kt
+++ app/src/main/java/com/fireblade/effectivecamera/graphics/CameraGLSurfaceView.kt
@@ -7,9 +7,13 @@ import android.os.Handler
 import android.os.Looper
 import android.util.AttributeSet
 import android.util.Size
+import android.view.SurfaceHolder
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.fireblade.effectivecamera.CameraActivity
 import com.fireblade.effectivecamera.graphics.common.RenderEffect
 import com.fireblade.effectivecamera.graphics.effects.EffectConfig
-
+import com.fireblade.effectivecamera.graphics.services.EffectViewModel
 
 class CameraGLSurfaceView(context: Context, attrs: AttributeSet) : GLSurfaceView(context, attrs) {
 
@@ -18,11 +22,25 @@ class CameraGLSurfaceView(context: Context, attrs: AttributeSet) : GLSurfaceView
   private var listenerHandler: Handler = Handler(Looper.getMainLooper())
   var surfaceEventListeners: List<IRendererEvents> = listOf()
 
+  private val effectViewModel: EffectViewModel by lazy {
+
+    (context as CameraActivity).let { fragmentActivity ->
+      ViewModelProviders.of(fragmentActivity)[EffectViewModel::class.java]
+    }
+  }
+
+  lateinit var effectObserver: Observer<EffectConfig>
+
   init {
     setEGLContextClientVersion(3)
     renderer = CameraGLRenderer(this)
     setRenderer(renderer)
     renderMode = RENDERMODE_WHEN_DIRTY
+  }
+
+  override fun surfaceDestroyed(holder: SurfaceHolder?) {
+    effectViewModel.getEffectConfig().removeObserver(effectObserver)
+    super.surfaceDestroyed(holder)
   }
 
   fun getPreviewSurfaceTex(): SurfaceTexture {
@@ -40,6 +58,10 @@ class CameraGLSurfaceView(context: Context, attrs: AttributeSet) : GLSurfaceView
   fun initializeEffect(effectConfig: EffectConfig) {
 
     renderer.initializeEffect(effectConfig)
+    effectObserver = Observer {
+      renderer.setEffectConfig(it)
+    }
+    effectViewModel.getEffectConfig().observeForever(effectObserver)
   }
 
   fun setRenderEffect(renderEffect: RenderEffect) {
